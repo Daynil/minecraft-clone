de7bfd0346b31ec3902a275aaa32a277a2fe2eff
Danny Libin <dlibinrx@gmail.com>
2022-01-16 11:10:54 -0600

    tighten player script, look sensitivity by input type

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,32 +7,48 @@ public class PlayerScript : MonoBehaviour
 {
 
   public float playerSpeed;
+
   public float mouseSensitivity;
+  public float rightGamepadSensitivity;
 
   public CharacterController controller;
   public Camera cam;
+  public PlayerInput input;
 
-  private Vector2 moveVec = Vector2.zero;
+  private Vector3 moveVec = Vector2.zero;
   private Vector2 lookVec = Vector2.zero;
 
   private float xRotation = 0f;
 
+  void Awake()
+  {
+    this.controller = this.GetComponent<CharacterController>();
+    this.cam = this.GetComponentInChildren<Camera>();
+    this.input = this.GetComponent<PlayerInput>();
+  }
+
   // Start is called before the first frame update
   void Start()
   {
-    // this.rb = this.GetComponent<Rigidbody>();
-    this.controller = this.GetComponent<CharacterController>();
-    this.cam = this.GetComponentInChildren<Camera>();
+
   }
 
   // Update is called once per frame
   void Update()
   {
-    Vector3 move = this.transform.right * moveVec.x + this.transform.forward * moveVec.y;
+    /*
+      We are performing vector addition on the normalized "left/right" vector of the player (x)
+      and the normalized forward/backward vector of the player (z)
+      ➡ + ⬆ = ↗ 
+      This determines the *direction* we want to move *relative to the rotation of the player*
+      Then, we multiply a fixed movement speed to determine the vector's magnitude
+    */
+    Vector3 move = this.transform.right * moveVec.x + this.transform.forward * moveVec.z;
     this.controller.Move(move * this.playerSpeed * Time.deltaTime);
 
-    float mouseX = lookVec.x * this.mouseSensitivity * Time.deltaTime;
-    float mouseY = lookVec.y * this.mouseSensitivity * Time.deltaTime;
+
+    float mouseX = lookVec.x * Time.deltaTime;
+    float mouseY = lookVec.y * Time.deltaTime;
 
     xRotation -= mouseY;
     xRotation = Mathf.Clamp(xRotation, -90, 90);
@@ -43,13 +59,19 @@ public class PlayerScript : MonoBehaviour
 
   void OnMove(InputValue value)
   {
-    this.moveVec = value.Get<Vector2>();
-    // this.rb.AddForce(value.Get<Vector2>() * 100);
+    Vector2 inputVec = value.Get<Vector2>();
+    // Map input 2d input vec to world 3d vec
+    // This essentially maps the up/down (y) inputs to forward/back (z) in world space
+    this.moveVec = new Vector3(inputVec.x, 0, inputVec.y);
   }
 
   void OnLook(InputValue value)
   {
+    // up down (y) on the mouse properly correspond to world up down (y), no need to map
     this.lookVec = value.Get<Vector2>();
+
+    float lookSensitivity = this.input.currentControlScheme == "Gamepad" ? this.rightGamepadSensitivity : this.mouseSensitivity;
+    this.lookVec = this.lookVec * lookSensitivity;
   }
 
   void OnJump(InputValue value)
@@ -60,6 +82,28 @@ public class PlayerScript : MonoBehaviour
   void OnFire(InputValue value)
   {
     // this.rb.AddForce(new Vector3(0, 0, 1000));
+  }
+
+  void OnControlsChanged()
+  {
+    // if (this.input is null)
+    // {
+    //   this.lookSensitivity = this.mouseSensitivity; // Null on initial check
+    //   return;
+    // }
+    // switch (this.input.currentControlScheme)
+    // {
+    //   case "Keyboard&Mouse":
+    //     this.lookSensitivity = this.mouseSensitivity;
+    //     break;
+    //   case "Gamepad":
+    //     this.lookSensitivity = this.rightGamepadSensitivity;
+    //     break;
+    //   default:
+    //     Debug.LogError($"Unhandled control scheme {this.input.currentControlScheme}");
+    //     this.lookSensitivity = this.mouseSensitivity;
+    //     break;
+    // }
   }
 }
 
